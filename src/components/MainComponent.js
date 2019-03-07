8ba2fc414e5c2a1ae23cd38107e83e5cb70179e4
Edgar Jeong <chjeong@e1z2r3p10.42.us.org>
2019-03-07 07:35:01 -0800

    Header, Footer, font-awesome added.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import DishdetailComponent from './DishdetailComponent.js';
-
+import Header from './HeaderComponent.js';
+import Footer from './FooterComponent.js';
 class Main extends Component {
 
   constructor(props) {
@@ -20,15 +21,12 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <Menu dishes={this.state.dishes} 
             onClick={(dishId) => this.onDishSelect(dishId)} />        
         <DishdetailComponent 
             dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer />    
       </div>
     );
   }
